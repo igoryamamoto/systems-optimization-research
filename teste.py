a15354ba1aa1819a2bdda9bc5bd3b5cc64e1fbd4
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-02-21 14:59:22 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 14:40:26 2017
+
+@author: Ígor Yamamoto
+"""
+
+from cvxopt import matrix, solvers
+Q = 2*matrix([ [2, .5], [.5, 1] ])
+p = matrix([1.0, 1.0])
+G = matrix([[-1.0,0.0],[0.0,-1.0]])
+h = matrix([0.0,0.0])
+A = matrix([1.0, 1.0], (1,2))
+b = matrix(1.0)
+sol=solvers.qp(Q, p, G, h, A, b)
+print(sol['x'])
