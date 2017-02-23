69c005762e38ffaa114bda57b77157d355526f01
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-02-23 16:54:52 -0300

    Create function to create matrix G

--- problem1.py
+++ problem1.py
@@ -7,6 +7,18 @@ Created on Thu Feb 23 10:58:03 2017
 import numpy as np
 from cvxopt import matrix, solvers
 
+def create_matrix_G(g, P, Nu):
+    g = np.append(g,np.zeros(Nu-1))
+    G = np.array([])
+    for i in range(P):
+        G = np.append(G,[g[i-j] for j in range(Nu)])  
+    return np.resize(G,(P,Nu))
+    
+# prediction horizon
+P = 15
+# control horizon
+Nu = 3
+
 g11 = np.array([
     -0.190000000000000,
     -0.380000000000000,
@@ -22,9 +34,7 @@ g11 = np.array([
     -2.28000000000000,
     -2.47000000000000,
     -2.66000000000000,
-    -2.85000000000000,
-    0,
-    0
+    -2.85000000000000
 ])
  
 g12 = np.array([
@@ -42,9 +52,7 @@ g12 = np.array([
     -0.781263905972892,
     -0.827190897644593,
     -0.870822030279379,
-    -0.912272072307002,
-    0,
-    0
+    -0.912272072307002
 ])
 
 g21 = np.array([
@@ -62,9 +70,7 @@ g21 = np.array([
     -0.239833619450740,
     -0.256029407050039,
     -0.271723817753356,
-    -0.286932372805971,
-    0,
-    0
+    -0.286932372805971
 ])
 
 g22 = np.array([
@@ -82,36 +88,15 @@ g22 = np.array([
     2.82000000000000,
     3.05500000000000,
     3.29000000000000,
-    3.52500000000000,
-    0,
-    0
+    3.52500000000000
 ])
-G11 = G12 = G21 = G22 = np.array([0,0,0])
-for i in range(np.size(g11)-2):
-    row = np.array([g11[i], g11[i-1], g11[i-2]])
-    G11 = np.vstack((G11,row))
-    
-    row = np.array([g12[i], g12[i-1], g12[i-2]])
-    G12 = np.vstack((G12,row))
-    
-    row = np.array([g21[i], g21[i-1], g21[i-2]])
-    G21 = np.vstack((G21,row))
-    
-    row = np.array([g22[i], g22[i-1], g22[i-2]])
-    G22 = np.vstack((G22,row))
-    
-G11 = np.delete(G11,0,0)
-G12 = np.delete(G12,0,0)
-G21 = np.delete(G21,0,0)
-G22 = np.delete(G22,0,0)    
+
+G11 = create_matrix_G(g11,P,Nu)
+G12 = create_matrix_G(g12,P,Nu)
+G21 = create_matrix_G(g21,P,Nu)
+G22 = create_matrix_G(g22,P,Nu) 
     
 G1 = np.hstack((G11,G12))
 G2 = np.hstack((G21,G22))
+
 G = np.vstack((G1,G2))
-
-
-
-
-
-
-
