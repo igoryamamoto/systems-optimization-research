b035ef670beefc47a6d898876708d1f940f5047c
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-09-17 17:42:04 -0300

    Add aux files

---
+++ src/aux/cvxopt_example.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 14:40:26 2017
+
+@author: Ígor Yamamoto
+"""
+
+# Example from "http://cvxopt.org/examples/tutorial/qp.html"
+from cvxopt import matrix, solvers
+Q = 2*matrix([ [2, .5], [.5, 1] ])
+p = matrix([1.0, 1.0])
+G = matrix([[-1.0,0.0],[0.0,-1.0]])
+h = matrix([0.0,0.0])
+A = matrix([1.0, 1.0], (1,2))
+b = matrix(1.0)
+sol=solvers.qp(Q, p, G, h, A, b)
+print(sol['x'])
