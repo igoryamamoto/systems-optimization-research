905b7ab0d74ed1b622b437c19c5ab3b1b4714584
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-10-26 13:47:47 -0200

    Sliding horizon test

--- src/ihmpc/ihmpc.py
+++ src/ihmpc/ihmpc.py
@@ -147,6 +147,27 @@ class OPOM(object):
         #plt.savefig('../../img/opom_step.png')
         plt.show()
         #return U, Y
+    
+    def output2(self, du1, du2, samples):
+        U = np.vstack((du1,du2)).T
+        X = np.zeros((samples+1,self.nx))
+        X[0] = self.X
+        Y = np.zeros((samples+1,self.ny))
+        Y[0] = self.C(0).dot(X[0])
+        for k in range(samples):
+            X[k+1] = self.A.dot(X[k]) + self.B.dot(U[k])
+            Y[k+1] = self.C(0).dot(X[k+1])
+        
+        self.X = X[samples]
+        return X, Y    
+        #for i in range(self.ny):
+        #    plt.plot(Y[:,i], label='y{}'.format(i+1))
+        #for i in range(self.nu):
+        #    plt.plot(U[1:,i], '--', label='u{}'.format(i+1))
+        #plt.legend(loc=4)
+        #plt.savefig('../../img/opom_step.png')
+        #plt.show()
+        #return U, Y
         
 class IHMPCController(OPOM):
     def __init__(self, H, Ts, m):
@@ -289,9 +310,10 @@ class IHMPCController(OPOM):
         
         H = H_m + H_inf
         
-        e_s = np.array([0, 0])
-        x_i = np.array([0.4, -0.4])
-        x_d = np.array([0, 0, 0, 0])
+        e_s = np.array([1 - 2.292925, 1 - 0.3075793])
+        
+        x_d = np.array([0, -1.39258457, 0.64501061, 0])
+        x_i = np.array([-0.133836  , -0.165534])
         
         
         cf_m = 0
@@ -305,14 +327,15 @@ class IHMPCController(OPOM):
         
         cf = cf_m + cf_inf
         beq =  np.hstack((e_s - self.m*self.Ts*x_i, -x_i)).T
-        #sol = solvers.qp(P=H, q=cf, A=self.Aeq, b=beq)
+        #sol = solvers.qp(P=matrix(H), q=matrix(cf), A=matrix(self.Aeq), b=matrix(beq))
         # minimize    (1/2)*x'*P*x + q'*x 
         # subject to  G*x <= h      
         #             A*x = b.
         #du = list(sol['x'])
         # s = sol['status']
         # j = sol['primal objective']
-        return H, cf
+        return H, cf, beq
+
 
 
 class Simulation(object):
@@ -327,13 +350,55 @@ if __name__ == '__main__':
     h12 = signal.TransferFunction([-1.7],[19.5, 1])
     h21 = signal.TransferFunction([-0.763],[31.8, 1])
     h22 = signal.TransferFunction([0.235],[1, 0])
-    H = [[h11, h12], [h21, h22]]
+    H1 = [[h11, h12], [h21, h22]]
     H2 = [[h11, h12, h11, h12], [h21, h22, h21, h22]]
     Ts = 1
     m = 3
-    controller = IHMPCController(H, Ts, m)
+    controller = IHMPCController(H1, Ts, m)
     controller2 = IHMPCController(H2, Ts, m)
     
+    o = OPOM(H1, Ts)
+    o.X = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+    o.X = np.array([0.286907, 1.1116616, 0, 0.29493999, -0.15978252, 0, 0.065493, 0.080981])
+    o.X = np.array([1.297874,  1.0319027,  0, -0.40238088,  0.17077504, 0, -0.008949  , -0.011092  ])
+    o.X = np.array([2.292925,  0.3075793 ,  0, -1.39258457, 0.64501061, 0, -0.133836  , -0.165534  ])
+    #du1 = np.array([-8.0603,7.2161,-0.1616])
+    #du2 = np.array([0.0628,10.3272,-5.5768])
+    
+    du1 = np.array([-3.0557,1.8628,0.8482])
+    du2 = np.array([0.5296,2.2075,-2.3925])
+    
+    du1 = np.array([-0.5151,-0.1428,1.0497])
+    du2 = np.array([0.2403,-0.1899,-0.4422])
+    
+    du1 = np.array([0.0795, -0.3145, 0.8923])
+    du2 = np.array([0.4348, -1.0189, -0.0731])
+    
+    du1 = np.array([-0.7687, 0.6665, 0.6310])
+    du2 = np.array([0.7352, -0.5667, -0.6973])
+
+    X, Y = o.output2(du1, du2, 3)
+    
+    #s1 = signal.step(h11,T=np.arange(100)*0.01+0.01)
+    #s2 = signal.step(h12,T=np.arange(100)*0.01+0.01)
+    #s3 = signal.step(h21,T=np.arange(100)*0.01+0.01)
+    #s4 = signal.step(h22,T=np.arange(100)*0.01+0.01)
+    #s1 = s1[1][-1]
+    #s2 = s2[1][-1]
+    #s3 = s3[1][-1]
+    #s4 = s4[1][-1]
+    #s11 = -0.18810000000000013
+    #s12 = -0.084153415443706123
+    #s21 = -0.023387828820529824
+    #s22 = 0.23265000000000013
+    #Y2 = np.zeros((4,2))
+    #Y2[0] = o.C(0).dot(X[0])
+    #for i in range(len(du1)):
+    #    Y2[i+1] = Y2[i] + [s11*du1[i] + s12*du2[i], s21*du1[i] + s22*du2[i]]
+    
+    print('X=\n',X)
+    print('Y=\n',Y)
+    #print('Y2=\n', Y2)
     
     A = controller.A
     B = controller.B
@@ -362,13 +427,18 @@ if __name__ == '__main__':
         u1 = pickle.load(f)
     with open('u2.pickle','rb') as f:
         u2 = pickle.load(f)
+        
     U = np.vstack((u1,u2)).T
     T = np.arange(tsim)
     controller.output(U, T)
     
     sim = Simulation(controller)
-    H, cf = sim.run()
-    print(H, '\n\n', cf)
+    
+    
+    
+    
+    H, cf, beq = sim.run()
+    #print(H, '\n\n', cf)
     
 '''   
     U2 = np.vstack((u1,u2,u1,u2)).T
